eb40180dd7221ef46be90bcfc8108e20c5fdb2be
Roberto Zanotto <robyzan8@gmail.com>
2015-10-08 18:55:31 +0200

    improved lim gob reader

--- lim_gob_read.go
+++ lim_gob_read.go
@@ -78,16 +78,12 @@ func (l *LimGobReader) Read(buf []byte) (total int, err error) {
 			// set err
 			return
 		}
-		n := copy(buf, p)
-		_, err = l.r.Discard(n)
-		if err != nil {
-			log.Fatal("we peeked #width, we should be able to discard n <= width")
-			// keep reader state consistent
-		}
+		var n int
+		n, err = l.r.Discard(copy(buf, p))
 		total += n
 		buf = buf[n:]
 		l.next = int(msgLen) + width - n // tooBig protects form overflow
-		if len(buf) == 0 {
+		if err != nil {
 			return
 		}
 	}
@@ -108,7 +104,7 @@ func (l *LimGobReader) Read(buf []byte) (total int, err error) {
 
 // ReadByte is provided so that gob understands that this reader is buffered;
 // see https://golang.org/pkg/encoding/gob/#NewDecoder
-// The implementation is not efficient, but gob doesn't actually call the function.
+// The implementation is not efficient, but gob doesn't actually call this function.
 func (l *LimGobReader) ReadByte() (c byte, err error) {
 	var b [1]byte
 	_, err = io.ReadFull(l, b[:])
