2a4112a26a4978e9bf28f1a4b3e89285aead971c
Roberto Zanotto <robyzan8@gmail.com>
2015-11-08 02:42:29 +0100

    made limGobReader private again. Hopefully we'll remove it when the message size limit becomes configurable from gob.Decoder

--- netchan_test.go
+++ netchan_test.go
@@ -1,12 +1,10 @@
-package netchan_test
+package netchan
 
 import (
 	"io"
 	"strconv"
 	"testing"
 	"time"
-
-	"github.com/robzan8/netchan"
 )
 
 // pipeConn represents one side of a full-duplex
@@ -29,10 +27,10 @@ func newPipeConn() (sideA, sideB pipeConn) {
 }
 
 // intProducer sends integers from 0 to n-1 on net-chan chName
-func intProducer(t *testing.T, man *netchan.Manager, chName string, n int) {
+func intProducer(t *testing.T, man *Manager, chName string, n int) {
 	go func() {
 		ch := make(chan int, 8)
-		err := man.Open(chName, netchan.Send, ch)
+		err := man.Open(chName, Send, ch)
 		if err != nil {
 			t.Error(err)
 		}
@@ -49,12 +47,12 @@ func intProducer(t *testing.T, man *netchan.Manager, chName string, n int) {
 
 // intConsumer drains net-chan chName, stores the received integers in a slice
 // and delivers the slice on a channel, which is returned
-func intConsumer(t *testing.T, man *netchan.Manager, chName string) <-chan []int {
+func intConsumer(t *testing.T, man *Manager, chName string) <-chan []int {
 	sliceCh := make(chan []int, 1)
 	go func() {
 		var slice []int
 		ch := make(chan int, 16)
-		err := man.Open(chName, netchan.Recv, ch)
+		err := man.Open(chName, Recv, ch)
 		if err != nil {
 			t.Error(err)
 		}
@@ -88,26 +86,26 @@ func checkIntSlice(t *testing.T, s []int) {
 // start the producer before the consumer
 func TestSendThenRecv(t *testing.T) {
 	sideA, sideB := newPipeConn()
-	intProducer(t, netchan.Manage(sideA), "integers", 100)
+	intProducer(t, Manage(sideA), "integers", 100)
 	time.Sleep(50 * time.Millisecond)
-	s := <-intConsumer(t, netchan.Manage(sideB), "integers")
+	s := <-intConsumer(t, Manage(sideB), "integers")
 	checkIntSlice(t, s)
 }
 
 // start the consumer before the producer
 func TestRecvThenSend(t *testing.T) {
 	sideA, sideB := newPipeConn()
-	sliceCh := intConsumer(t, netchan.Manage(sideB), "integers")
+	sliceCh := intConsumer(t, Manage(sideB), "integers")
 	time.Sleep(50 * time.Millisecond)
-	intProducer(t, netchan.Manage(sideA), "integers", 100)
+	intProducer(t, Manage(sideA), "integers", 100)
 	checkIntSlice(t, <-sliceCh)
 }
 
 // open many chans in both directions
 func TestManyChans(t *testing.T) {
 	sideA, sideB := newPipeConn()
-	manA := netchan.Manage(sideA)
-	manB := netchan.Manage(sideB)
+	manA := Manage(sideA)
+	manB := Manage(sideB)
 	var sliceChans [100]<-chan []int
 	for i := range sliceChans {
 		chName := "integers" + strconv.Itoa(i)
@@ -132,91 +130,7 @@ func TestManyChans(t *testing.T) {
 // TODO: find a better way of testing this
 func TestCredits(t *testing.T) {
 	sideA, sideB := newPipeConn()
-	intProducer(t, netchan.Manage(sideA), "integers", 1000)
-	s := <-intConsumer(t, netchan.Manage(sideB), "integers")
+	intProducer(t, Manage(sideA), "integers", 1000)
+	s := <-intConsumer(t, Manage(sideB), "integers")
 	checkIntSlice(t, s)
-}
-
-func TestLimGobReader(t *testing.T) {
-	sideA, sideB := newPipeConn()
-	go sliceProducer(t, sideA)
-	sliceConsumer(t, sideB)
-}
-
-const (
-	limit     = 100 // size limit enforced by sliceConsumer
-	numSlices = 100 // number of slices to send
-)
-
-// sliceProducer sends on "slices". The last slice will be too big.
-func sliceProducer(t *testing.T, conn io.ReadWriteCloser) {
-	mn := netchan.Manage(conn)
-	ch := make(chan []byte, 1)
-	err := mn.Open("slices", netchan.Send, ch)
-	if err != nil {
-		t.Error(err)
-	}
-	small := make([]byte, limit-10)
-	big := make([]byte, limit+5)
-	for i := 1; i <= numSlices; i++ {
-		slice := small
-		if i == numSlices {
-			slice = big
-		}
-		select {
-		case ch <- slice:
-		case <-mn.ErrorSignal():
-			t.Error(mn.Error())
-		}
-	}
-	close(ch)
-}
-
-// limitGobConn applies a LimGobReader to a connection
-func limitGobConn(conn io.ReadWriteCloser, n int) io.ReadWriteCloser {
-	return struct {
-		*netchan.LimGobReader
-		io.WriteCloser
-	}{
-		netchan.NewLimGobReader(conn, n),
-		conn,
-	}
-}
-
-// sliceConsumer receives from "slices" using a LimGobReader.
-// The last slice is too big and must generate errSizeExceeded
-func sliceConsumer(t *testing.T, conn io.ReadWriteCloser) {
-	l := limitGobConn(conn, limit)
-	if _, ok := l.(io.ByteReader); !ok {
-		t.Error("result of limitGobConn is not an io.ByteReader")
-	}
-	mn := netchan.Manage(l)
-	// use a receive channel with capacity 1, so that items come
-	// one at a time and we get the error for the last one only
-	ch := make(chan []byte, 1)
-	err := mn.Open("slices", netchan.Recv, ch)
-	if err != nil {
-		t.Error(err)
-	}
-	for i := 1; i <= numSlices; i++ {
-		if i < numSlices {
-			select {
-			case <-ch:
-			case <-mn.ErrorSignal():
-				t.Error(mn.Error())
-			}
-			continue
-		}
-		// i == numSlices, expect errSizeExceeded
-		select {
-		case <-ch:
-			t.Error("LimGobReader did not block too big message")
-		case <-mn.ErrorSignal():
-			err := mn.Error()
-			if err.Error() == "netchan LimGobReader: message size limit exceeded" {
-				return // success
-			}
-			t.Error(err)
-		}
-	}
 }
